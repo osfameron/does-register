f92c98958218aa822b2951ee6d7c20d7bfd2eb10
Hakim Cassimally <hakim@doesliverpool.com>
2014-04-13 22:58:21 +0000

    Set up fixtures in testing module

--- t/db.t
+++ t/db.t
@@ -10,16 +10,7 @@ use DoESTest;
 
 my $app = DoESTest->new;
 
-warn $app->dsn;
-
 my $db = $app->db;
-
-$db->resultset('Type')->create({ name => 'orga' });
-$db->resultset('Type')->create({ name => 'perm', unlimited => 1 });
-$db->resultset('Type')->create({ name => 'ws', unlimited => 1 });
-$db->resultset('Type')->create({ name => 'friend' });
-$db->resultset('Type')->create({ name => 'fwb' });
-
 is $db->resultset('Type')->count, 5;
 
 done_testing;
--- t/lib/DoESTest.pm
+++ t/lib/DoESTest.pm
@@ -3,13 +3,16 @@ package DoESTest;
 use Moo;
 extends 'DoES::Register';
 use Test::PostgreSQL;
+use FindBin;
 
 has dsn => (
     is => 'lazy',
     # isa => 'Str',
     default => sub {
         my $self = shift;
-        return $ENV{DOES_TEST_DSN} ||= $self->test_postgresql->dsn;
+        my $dsn = $ENV{DOES_TEST_DSN} ||= $self->test_postgresql->dsn;
+        print STDERR "# DSN: $dsn\n";
+        return $dsn;
     }
 );
 
@@ -21,12 +24,26 @@ has test_postgresql => (
     }
 );
 
+sub setup_fixtures {
+    # I never got on with DBIC::Fixtures, I just want to dump *everything* not
+    # have to fiddle with configs
+    my ($self, $db) = @_;
+
+    my $type_rs = $db->resultset('Type');
+    $type_rs->create({ name => 'orga' });
+    $type_rs->create({ name => 'perm', unlimited => 1 });
+    $type_rs->create({ name => 'ws', unlimited => 1 });
+    $type_rs->create({ name => 'friend' });
+    $type_rs->create({ name => 'fwb' });
+}
+
 around db => sub {
     my $orig = shift;
     my $self = shift;
     my $db = $self->$orig(@_);
     $db->deploy unless $ENV{DOES_TEST_DB_DEPLOYED}++;
-    $db;
+    $self->setup_fixtures($db);
+    return $db;
 };
 
 1;
